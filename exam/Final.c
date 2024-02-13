507129166fc82ffd4b55a1912ba8e7ccddaf50e0
neon <neonlightgames@hotmail.com>
2024-02-13 14:28:26 -0600

    Mejoras

--- exam/Final.c
+++ exam/Final.c
@@ -21,57 +21,68 @@
 
 #include <stdio.h>
 
+#define MAX 100
+
 int main ()
 {
-    int opc, result; // Variables opcion 2
+    int opc, valid, last; // Variables de menu y vadilacion
+    const char linea1[]="********************************", linea2[]="--------------------------------"; // Lineas de menu
     int cont,cal,califs[100],j,respaldo; // Variables para la opción 3
-    int n,i,bandera;// Variables para la opción 4
-    const char linea1[]="******************************", linea2[]="------------------------------";
 
     do {
         printf ("\n%s", linea1);
-        printf ("\n* 1.   Temperatura           *");
-        printf ("\n* 2.   Multiplos             *");
-        printf ("\n* 3.   Arreglos              *");
-        printf ("\n* 4.   Cadenas               *");
-        printf ("\n* 5.   Salir                 *");
+        printf ("\n* 1.   Temperatura             *");
+        printf ("\n* 2.   Multiplos               *");
+        printf ("\n* 3.   Arreglos                *");
+        printf ("\n* 4.   Cadenas                 *");
+        printf ("\n* 5.   Salir                   *");
         printf ("\n%s", linea1);
 
-        /* Validacion de la opcion, para evitar errores */
-        /* Debe mejorarse, acepta cadenas que empiezan en un número (no deberia de suceder)*/
+        // Ciclo que valida la entrada
         do {
             printf ("\n Opcion: ");
-            result = scanf ("%d",&opc);
-            if (result==0 || opc<1 || opc>5)  {
-                printf ("\n Ingresa un n%cmero valido, los caracteres no son aceptados\n",163);
-                while (getchar() != '\n');
-            }
-        } while (result==0 || opc<1 || opc>5);
+            valid = scanf ("%d",&opc);
 
-        printf ("%s", linea2);
+            if (valid==0) {
+                printf ("\n Las cadenas y caracteres no son aceptados\n");
+                while (getchar() != '\n') {}
+            }
+            else if ((last = getchar()) != '\n') {
+                printf ("\n Las cadenas y caracteres no son aceptados\n");
+                while (getchar() != '\n') {}
+                valid = 0;
+            }
+        } while (valid==0);
+
+        if (opc >= 1 && opc <= 5)
+            printf ("%s\n", linea2);
 
         switch (opc) { // Opciones
             case 1:
                 float temp;
 
-                /* Validacion de una temperatura, dada en números */
-                /* Debe mejorarse, acepta cadenas que empiezan en un número (no deberia de suceder)*/
+                // Ciclo que valida la entrada
                 do {
                     printf ("\n Temperatura del sujeto: ");
-                    result = scanf ("%f",&temp);
+                    valid = scanf ("%f",&temp);
 
-                    if (result==0) {
-                        printf ("\n Ingresa un valor valido, sin caracteres desconocidos\n");
-                        while (getchar() != '\n');
+                    if (valid==0) {
+                        printf ("\n Las cadenas y caracteres no son aceptados\n");
+                        while (getchar() != '\n') {}
                     }
-                } while (result==0);
+                    else if ((last = getchar()) != '\n') {
+                        printf ("\n Las cadenas y caracteres no son aceptados\n");
+                        while (getchar() != '\n') {}
+                        valid = 0;
+                    }
+                } while (valid==0);
 
                 if (temp<35.0)
-                    printf ("\n El sujeto tiene hipotermia");
+                    printf ("\n El sujeto tiene hipotermia\n", temp, 248);
                 else if (temp<=37.0)
-                    printf ("\n La temperatura es normal");
+                    printf ("\n La temperatura es normal\n", temp, 248);
                 else
-                    printf ("\n El sujeto tiene hipertemia");
+                    printf ("\n El sujeto tiene hipertermia\n", temp, 248);
 
                 break;
 
@@ -82,7 +93,7 @@ int main ()
             case 3:
                 cont=1;
                 j=0;
-                    printf("\nIngrese las calificaciones\n");
+                printf("\n Ingrese las calificaciones\n");
                 printf("1. ");
                 scanf("%i",&cal);
                 while(cal>500 && cont<100){
@@ -112,59 +123,67 @@ int main ()
                 };
 	            break;
 
-            case 4: {
-            	printf("%s\n",linea2);
-					//Lectura y comprobacion de tamaño de las cadenas
-					do {
-						printf("Ingrese primera cadena\n");
-						scanf("%s",&cad1);
-						while (getchar() != '\n'){}
-						i=0;n=0;
-						while(cad1[i]!='\0') {
-							i=i+1;
-							n=n+1;	
-						}
-						if (i>32 || i<1) {
-							printf("\a\n*Tama%co invalido*\n",(char)164);
-						}
-					}while (i>32);	
-					printf("\n");
-					do {
-						printf("Ingrese segunda cadena\n");
-						scanf("%s",&cad2);
-						while (getchar() != '\n'){}
-						i=0;
-						while(cad2[i]!='\0') {
-							i=i+1;	
-						}
-						if (i>32 || i<1) {
-							printf("\a\n*Tama%co invalido*\n",(char)164);
-						}
-					}while (i>32);		
-					//Comparacion de las dos cadenas
-					bandera=1;
-					for(i=0;i<n;i++) {
-						if(cad1[i]!=cad2[i]) {
-							bandera=0;
-						}	
-					}
-					//Resultado de la comprobación
-					if (bandera==1) {
-						printf("\n*Las cadenas son iguales*\n");
-					}
-					else {
-						printf("\n*Las cadenas no son iguales*\n");
-					}
+            case 4:
+                int n, i, bandera;
+                char cad1[32], cad2[32];
+                
+                // Lectura y comprobacion de tamaño de las cadenas
+                do {
+                    printf("\n Ingrese primera cadena\n ");
+                    scanf("%s",&cad1);
+                    while (getchar() != '\n'){}
+                    i=0; n=0;
+                    while(cad1[i]!='\0') {
+                        i=i+1;
+                        n=n+1;	
+                    }
+                    if (i>32 || i<1) {
+                        printf("\a\n *Tama%co invalido*\n",(char)164);
+                    }
+                } while (i>32);	
+
+                printf("\n");
+
+                do {
+                    printf(" Ingrese segunda cadena\n ");
+                    scanf("%s",&cad2);
+                    while (getchar() != '\n'){}
+                    i=0;
+                    while(cad2[i]!='\0') {
+                        i=i+1;	
+                    }
+                    if (i>32 || i<1) {
+                        printf("\a\n *Tama%co invalido*\n",(char)164);
+                    }
+                }while (i>32);	
+
+                // Comparacion de las dos cadenas
+                bandera=1;
+                for(i=0;i<n;i++) {
+                    if(cad1[i]!=cad2[i]) {
+                        bandera=0;
+                    }	
+                }
+                // Resultado de la comprobación
+                if (bandera==1) {
+                    printf("\n *Las cadenas son iguales*\n");
+                }
+                else {
+                    printf("\n *Las cadenas no son iguales*\n");
+                }
 				break;
-				}
 
             case 5:
                 printf ("\n Hasta luego, vuelva pronto\n");
                 break;
 
+            default:
+                printf ("\n Opcion invalida");
+                break;
         }
 
-        printf ("\n%s\n", linea2);
+        if (opc >= 1 && opc <= 5)
+            printf ("\n%s\n", linea2);
 
     } while (opc!=5);
 
