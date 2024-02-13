94ee0a505cd1c96c99a012edec2e66afddc7b389
neon <neonlightgames@hotmail.com>
2024-02-12 19:35:25 -0600

    Parche

--- practices/5/P1.4_Insercion_binaria.c
+++ practices/5/P1.4_Insercion_binaria.c
@@ -5,45 +5,53 @@
 
 #include <stdio.h>
 
-#define MAX 100
+#define MAX 101
 
 int main ()
 {   
-    int i, x, L, R, m, n, j, y, arr[MAX];
+    int i, value, left, right, medium, size, j, y, arr[MAX];
 
     printf ("\n Tamano del arreglo: ");
-    scanf ("%d",&n);
+    scanf ("%d",&size);
     printf ("\n Leamos el arreglo:\n");
 
-    for (y=0; y<n; y++) {
-        printf (" Valor %d: ",y+1);
+    for (y=0; y<size; y++) {
+        printf (" [%d]: ",y+1);
         scanf ("%d",&arr[y]);
     }
 
     /* Ordenamiento */
-    for (i=2; i<n; i++) {
-        x= arr[i];
-        L= 1;
-        R= i;
+    for (i=1; i<size; i++) {
+        value = arr[i];
+        left = 0;
+        right = i-1;
 
-        while (L<R) {
-            m= (L+R)/(2);
-            if (arr[m] <= x)
-                L= m+1;
+        /* 
+            Mientras el item a la izquierda sea menor o igual al de la derecha
+            * Se busca la mitad
+            * Si el item de la mitad del arreglo es mayor al item guardado
+              se reduce la mitad en 1
+            * Si el item de la mitad del arreglo es menor o igual al item
+              guardado, se aumenta la mitad a 1
+        */
+        while (left<=right) {
+            medium = (left+right)/2;
+            if (arr[medium] > value)
+                right = medium-1;
             else
-                R= m;
+                left = medium+1;
         }
 
-        for (j=i; j<R+1; j--) {
-            arr[j]= arr[j-1];
-        }
-
-        arr[R]= x;
+        /* Mueve los valores a sus lugares ideales */
+        for (j=i-1; j>=left; j--)
+            arr[j+1] = arr[j];
+        /* Asisna el valor en memoria al primer lugar disponible */
+        arr[left] = value;
     }
 
     printf ("\n El arreglo ordenado es: ");
 
-    for (y=0; y<n; y++)
+    for (y=0; y<size; y++)
         printf ("%d, ",arr[y]);
 
     return 0;
