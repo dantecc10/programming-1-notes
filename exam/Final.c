011455592e83f9fde3ad36a4ddc7ccae8c106459
NobaliFM <142608794+NobaliFM@users.noreply.github.com>
2024-02-15 00:52:24 -0600

    Update Final.c

--- exam/Final.c
+++ exam/Final.c
@@ -1,3 +1,24 @@
+/*
+    Programa final con todo lo solicitado
+
+    1. Temperatura: Determinará la respuesta: de hipotermia sí la temperatura de una persona es menor de 35 °C,
+       de estar en el intervalo 36°C±1.0°C considérese normal, y si es mayor de 37.0 °C existe hipertermia (hay fiebre).
+
+    2. Dado un número n, y un límite, sumar los números múltiplos de n, menores a límite.
+       Por ejemplo: Si n=5 y límite=23, se sumarán los números 5+10+15+20=50
+
+    3. Leer las calificaciones del examen de ingreso a la BUAP mientras el valor sea mayor a 500; y al terminar
+       presente el arreglo ordenado y dé la calificación más alta y la más baja del conjunto de calificaciones.
+       Realice la prueba de ejecución para los siguientes datos:
+          758 745 698 802 783 711 822 799 815 875
+          699 840 796 723 741 800 769 700 810 459
+
+    4. Escribir un programa que lea dos cadenas de caracteres, las compare e infor me de si son iguales o diferentes.
+       No usar la función de la biblioteca estándar strcmp().
+
+    5. Salir del programa (Hasta que no se solicite repetir constantemente)
+*/
+
 #include <stdio.h>
 
 #define LINE1 "   **************************"
