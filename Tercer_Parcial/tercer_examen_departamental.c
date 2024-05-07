a3d0cd72f6ee46b99d0bb03045779a639452c0e3
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-05-07 15:38:20 -0600

    [ UPDATES ] - Add function (guardar_datos) - Add commits - Fix struct declaration

--- Tercer_Parcial/tercer_examen_departamental.c
+++ Tercer_Parcial/tercer_examen_departamental.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #define MAX 20
 
+// Variables globales
 int i;
 
 struct jugadores{
@@ -11,7 +12,7 @@ struct jugadores{
 	int bandera;
 };
 
-void menu(){
+void menu() {
 	printf("\n------------ MENU ------------\n");
 	printf("1. Altas\n2. Mostrar jugadores en orden\n3. Buscar un jugador\n");
 	printf("4. Eliminar un jugador\n5. Salir\n\nR: ");
@@ -19,19 +20,32 @@ void menu(){
 
 int buscar_jugador(struct jugadores c[], char nickname_buscar[MAX],int *donde){
 	for (i=0;i<MAX;i++){
-		if (c[i].bandera!=0 && (strcmp(c[i].nickname,nickname_buscar))==0){
+		if (c[i].bandera!=0 && (strcmp(c[i].nickname,nickname_buscar))==0) {
 			*donde=i;  //posicion donde se encuentra el jugador buscado
 			return 1;  //se encuentra 
-		};
-	};
-	return 0;   //no  se encuentra
+		}
+	}
+	return 0; //no  se encuentra
 }
 
-//------------------
+void guardar_datos(struct jugadores var[]) // Guarda el Nickname y vidas restantes de cada jugador en un archivo txt 
+{
+	FILE *players;
+	players = fopen("PlayersData.txt", "w");
+
+	for (i=0; i<MAX; i++)
+	{
+		if (var[i].bandera != 0 && (strlen(var[i].nickname)) > 0)
+			fprintf(players, "%c - %d\n", var[i].nickname, var[i].vidas);
+	}
+}
+
+// ----------------------------------------------------------------------------------------------------
+
 int main(){
 	int opc,donde; //declaracion de variables
 	char nickname_buscar[MAX];
-	jugadores lista[MAX];
+	struct jugadores lista[MAX];
 	FILE *jugadores_arch;
 	
 	jugadores_arch=fopen("Jugadores_:)","rb"); //para la recuperación de datos en el archivo binario
@@ -40,46 +54,54 @@ int main(){
 		fread(lista, sizeof(struct jugadores), MAX, jugadores_arch);
                 fclose(jugadores_arch);
 	}else{ // no  existe el archivo binario
-		for(i=0;i<MAX;i++){
+		for (i=0;i<MAX;i++) {
 		    lista[i].bandera=0; // se inicializan todas las bandera en 0, 
-                };                      //indicando que todas las posiciones están desocupadas
+		};                      //indicando que todas las posiciones están desocupadas
 	}; 
 	
 	do{
 		menu();
 		scanf("%i",&opc);
 		switch(opc){
-			case 1: break;
+			case 1: // Alta
+				break;
 		  
-			case 2: break;
+			case 2: // Mostrar
+				break;
 			
-			case 3://buscar jugador
-			     printf("\nNickname del jugador: ");
-			     scanf(" %[^\n]s",nickname_buscar);
-			     if(buscar_jugador(lista,nickname_buscar,&donde)){
-					 printf("\nEncontrado!\n\n");
-					 printf("Nickname: %s \n",lista[donde].nickname);
-					 printf("Nivel: %i\n",lista[donde].nivel);
-					 printf("Vidas: %i\n",lista[donde].vidas);	 
-			     }else{
-				     printf("\nJugador no encontrado\n");
-			     };
-			     break;
+			case 3: // Buscar jugador
+				printf("\nNickname del jugador: ");
+				scanf("%[^\n]s",nickname_buscar);
+				if(buscar_jugador(lista,nickname_buscar,&donde)){
+					printf("\nEncontrado!\n\n");
+					printf("Nickname: %s \n",lista[donde].nickname);
+					printf("Nivel: %i\n",lista[donde].nivel);
+					printf("Vidas: %i\n",lista[donde].vidas);	 
+				}else{
+					printf("\nJugador no encontrado\n");
+				};
+				break;
 			
-			case 4: break;
+			case 4: // Eliminar
+				break;
 			
-			case 5: 
-			      printf("\nSaliendo...");
-			      break;
+			case 5: // Salir
+				printf("\nSaliendo...");
+				break;
 			
-		        default:
-			     printf("Opcion invalida\n"); break;
+			default:
+				printf("Opcion invalida\n");
+				break;
 		};
 			
-	}while(opc!=5);
-	//para guardar los datos en el archivo binario
-	jugadores_arch=fopen("Jugadores_:)","wb"); 
+	} while(opc!=5);
+	
+	// Para guardar los datos en el archivo binario
+	jugadores_arch = fopen("Jugadores_:)", "wb"); 
 	fwrite(lista, sizeof(struct jugadores), MAX, jugadores_arch); //se escribe en el archivo binario
 	fclose(jugadores_arch);
+
+	// Para guardar en el archivo de texto
+	guardar_datos(lista);
 }
 
