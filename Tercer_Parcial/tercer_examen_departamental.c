0423818546163bbe34f8118b1ec9b3a609ee1182
NobaliFM <142608794+NobaliFM@users.noreply.github.com>
2024-05-08 23:23:34 -0600

    Update tercer_examen_departamental.c

--- Tercer_Parcial/tercer_examen_departamental.c
+++ Tercer_Parcial/tercer_examen_departamental.c
@@ -18,25 +18,25 @@ void menu() {
 	printf("4. Eliminar un jugador\n5. Salir\n\nR: ");
 }
 
-
 void alta_jugador(struct jugadores c[]){
 	int disponibilidad=0;
 	i=0;
 	do { //Checa espacio disponible en estructura
 		if(c[i].bandera==0){
 			disponibilidad=1;
-		}
+		};
 		i++;
 	} while((disponibilidad==0)&&(i<MAX));
 	
 	if (disponibilidad==1) {//Agrega jugador en espacio disponible
 		i--;
-		printf("Ingrese nickname: ");
-		scanf("%[^\n]s",c[i].nickname);
+		printf("\nIngrese nickname: ");
+		scanf(" %[^\n]s",c[i].nickname);
 		printf("Ingrese nivel: ");
 		scanf("%i",&c[i].nivel);
 		printf("Ingrese numero de vidas: ");
 		scanf("%i",&c[i].vidas);
+		c[i].bandera=1;
 	}
 	else {
 		printf("No hay espacio disponible");
@@ -53,20 +53,65 @@ int buscar_jugador(struct jugadores c[], char nickname_buscar[MAX],int *donde){
 	return 0; //no  se encuentra
 }
 
-void guardar_datos(struct jugadores var[]) // Guarda el Nickname y vidas restantes de cada jugador en un archivo txt 
-{
+void guardar_datos(struct jugadores var[]) // Guarda el Nickname y vidas restantes
+{                                          // de cada jugador en un archivo txt 
 	FILE *players;
 	players = fopen("PlayersData.txt", "w");
-
-	for (i=0; i<MAX; i++)
-	{
-		if (var[i].bandera != 0 && (strlen(var[i].nickname)) > 0)
-			fprintf(players, "%c - %d\n", var[i].nickname, var[i].vidas);
-	}
-	fclose(players);
+    if(players != NULL){
+	    for (i=0; i<MAX; i++)
+	    {
+		    if (var[i].bandera != 0 && (strlen(var[i].nickname)) > 0)
+			    fprintf(players, "%s - %d\n", var[i].nickname, var[i].vidas);
+	    }
+	    fclose(players);
+    }else{
+		printf("Error en el archivo\n");
+	};
 }
 
-// ----------------------------------------------------------------------------------------------------
+void ordenar_max_min(int cadenita[], int total,struct jugadores c[]){//ordenar el arreglo array e
+    int j,respaldo;                                                  //imprimir los jugadores
+    for (i = 0; i < total - 1; i++) {// Selection Sort
+        for (j = i + 1; j < total; j++) {
+            if (cadenita[j] > cadenita[i]) {
+               respaldo = cadenita[i];
+               cadenita[i] = cadenita[j];
+               cadenita[j] = respaldo;
+            };
+        };
+     };//
+     
+     printf("\nJugadores ordenados de mayor a menor\n\n");
+     for (i=0;i<total;i++){//para recorrer el arreglo array
+		 for(int p=0;p<MAX;p++){//para recorrer todo el arreglo de jugadores
+			 if(c[p].bandera!=0 && c[p].nivel==cadenita[i]){
+				 printf(">> %s --- %d\n",c[p].nickname,c[p].nivel);
+			 };
+		 };
+     };
+}
+
+int duplicado(int numero, int array[MAX],int total){
+	int p;
+	for (p=0;p<total;p++){ //para saber si el numero ya está en el arreglo
+		if(numero == array[p])
+			return 0;// se encuentra
+	};
+	return 1;// no se encuentra
+}
+
+void mostrar_j(struct jugadores c[]){
+	int array[MAX],j=1;
+	array[0]=c[0].nivel;
+	for (i=1;i<MAX;i++){//para ingresar las distintas vidas al arreglo array
+		if(c[i].bandera == 1 && duplicado(c[i].nivel,array,j)){
+			array[j]=c[i].nivel;
+			j++;
+		};
+	};
+	ordenar_max_min(array,j,c);
+}
+// ------------------------------------------------------------------------------------------
 
 int main(){
 	int opc,donde; //declaracion de variables
@@ -74,8 +119,8 @@ int main(){
 	struct jugadores lista[MAX];
 	FILE *jugadores_arch;
 	
-	jugadores_arch=fopen("Jugadores_:)","rb"); //para la recuperación de datos en el archivo binario
-	
+	jugadores_arch=fopen("Jugadores_:)","rb"); //para la recuperación de datos
+	                                           
 	if(jugadores_arch != NULL){ //existe el archivo binario
 		fread(lista, sizeof(struct jugadores), MAX, jugadores_arch);
                 fclose(jugadores_arch);
@@ -94,11 +139,12 @@ int main(){
 				break;
 		  
 			case 2: // Mostrar
+			    mostrar_j(lista);
 				break;
 			
 			case 3: // Buscar jugador
 				printf("\nNickname del jugador: ");
-				scanf("%[^\n]s",nickname_buscar);
+				scanf(" %[^\n]s",nickname_buscar);
 				if(buscar_jugador(lista,nickname_buscar,&donde)){
 					printf("\nEncontrado!\n\n");
 					printf("Nickname: %s \n",lista[donde].nickname);
@@ -110,6 +156,14 @@ int main(){
 				break;
 			
 			case 4: // Eliminar
+			    printf("\nNickname del jugador a eliminar: ");
+				scanf(" %[^\n]s",nickname_buscar);
+				if(buscar_jugador(lista,nickname_buscar,&donde)){
+					lista[donde].bandera=0;
+					printf("\nJugador eliminado con exito ...\n");	 
+				}else{
+					printf("\nJugador no encontrado\n");
+				};
 				break;
 			
 			case 5: // Salir
@@ -130,4 +184,3 @@ int main(){
 	// Para guardar en el archivo de texto
 	guardar_datos(lista);
 }
-
